6f96bc25489ebb77b2fb234ecf548a48ad72482d
Sideral TI <sideralti.app@gmail.com>
2025-01-23 14:47:18 -0300

    initial project

---
+++ src/main/java/com/sideralti/app/SoftwareSpringDienteBlancoDentalClinicApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSpringDienteBlancoDentalClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSpringDienteBlancoDentalClinicApplication.class, args);
+	}
+
+}
